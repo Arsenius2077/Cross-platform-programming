1645dbc6cb71ad4aac4fdd9dfa0163e00e096641
Arsenii Poliovyi <55910758+Arsenius2077@users.noreply.github.com>
2022-12-06 10:41:20 +0200

    Add files via upload

---
+++ Lab_4/Lab1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Lab_4
+{ 
+    internal class Lab1
+    {
+        public int lab1(string pathI)
+        {
+            int[,] a = new int[13, 13];
+            int[] b = new int[13];
+            int[] c = new int[13];
+            int k = 0, n = 0, col = 0, t = 0;
+
+
+            int s(int n, int k)
+            {
+                if (k == 0) return 1;
+                else return a[n, k];
+            }
+
+            string path = @"input.txt";
+            bool fileExist = File.Exists(path);
+            if (!fileExist)
+            {
+                Console.WriteLine("File does not exist");
+            }
+            string text = File.ReadAllText("input.txt");
+            var textMatrix = text.Split("\r\n");
+            var firstTextArray = textMatrix[0].Split(" ");
+            n = Convert.ToInt32(firstTextArray[0]);
+
+            if (n >= 13)
+            {
+                Console.WriteLine("N must be <= 12");
+            }
+            k = Convert.ToInt32(firstTextArray[1]);
+
+            if (k <= 0 || k >= n)
+            {
+                Console.WriteLine("K must be >= 0 and <= N");
+            }
+            var secondTextArray = textMatrix[1].Split(" ");
+
+            for (int i = 1; i <= 12; i++)
+            {
+                a[i, 1] = i;
+                for (int j = 2; j <= i; j++)
+                    a[i, j] = a[i, j - 1] * (i - j + 1);
+            }
+
+            for (int i = 1; i <= k; i++)
+            {
+                b[i] = Convert.ToInt32(secondTextArray[i - 1]);
+                t = 0;
+                for (int j = 1; j <= b[i]; j++)
+                    if (c[j] == 0) t++;
+                col = col + (t - 1) * s(n - i, k - i);
+                c[b[i]] = 1;
+            }
+
+            File.WriteAllText("output.txt", (col + 1).ToString());
+            return 0;
+        }   
+    }
+}
+    
---
+++ Lab_4/Lab2.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System;
+using System.Linq;
+using System.IO;
+
+namespace Lab_4
+{
+    internal class Lab2
+    {
+        public int lab2(string pathI)
+        {
+            long n, a, b, c;
+            long[] mas = new long[5002];
+
+            string inputFilePath = @"input.txt";
+            bool fileExist = File.Exists(inputFilePath);
+            if (!fileExist)
+            {
+                Console.WriteLine("File does not exist");
+                return -1;
+            }
+
+            var fileContent = File.ReadAllText("input.txt");
+            if (fileContent.Length == 0)
+            {
+                Console.WriteLine("Input data is empty try again");
+                return -1;
+            }
+            var contentMatrix = fileContent.Split("\r\n");
+            var peopleQueueSize = Convert.ToInt32(contentMatrix[0]);
+
+
+            for (int i = 1; i <= peopleQueueSize + 2; i++)
+            {
+                mas[i] = 20000000;
+            }
+            for (int i = 1; i <= peopleQueueSize; i++)
+            {
+                var contentLine = Array.ConvertAll(contentMatrix[i].Split(" "), s => int.Parse(s));
+                if (contentLine.Length != 3)
+                {
+                    Console.WriteLine("Invalid input data try again");
+                    return -1;
+                }
+                a = contentLine[0];
+                b = contentLine[1];
+                c = contentLine[2];
+                Console.WriteLine(a + " " + b + " " + c + "\n");
+                if (mas[i] > a + mas[i - 1])
+                {
+                    mas[i] = a + mas[i - 1];
+                }
+                if (mas[i + 1] > b + mas[i - 1])
+                {
+                    mas[i + 1] = b + mas[i - 1];
+                }
+                if (mas[i + 2] > c + mas[i - 1])
+                {
+                    mas[i + 2] = c + mas[i - 1];
+                }
+            }
+            Console.WriteLine(mas[peopleQueueSize]);
+
+            var outputFilePath = @"output.txt";
+            var outputFile = File.Create(outputFilePath);
+            outputFile.Write(Encoding.Default.GetBytes(mas[peopleQueueSize].ToString()));
+            outputFile.Close();
+            return 0;
+        }
+    }
+}
